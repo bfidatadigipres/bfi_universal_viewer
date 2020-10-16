aa0750130a65911024f5c1c7463077d8d5701e3d
Stephen <stephen.mcconnachie@bfi.org.uk>
2020-10-16 16:48:04 +0100

    Created uv_start.sh

---
+++ uv_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+count=$(pgrep -fc test_universal_viewer)
+
+if (( $count > 0))
+then
+    echo "Universal Viewer is running" > runlog
+else
+    echo "Universal Viewer is not running - starting it now" > runlog
+    cd /home/datadigipres/code/test_universal_viewer && npm start
+fi
