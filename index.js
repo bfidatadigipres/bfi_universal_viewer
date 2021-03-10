b9a381f7b9cc9b5de74156ff9e1a106b81a9607c
Daniel Grant <daniel.grant@digirati.com>
2021-03-10 12:00:30 +0000

    Rough cut of logging events in the UV

--- index.js
+++ index.js
@@ -1,102 +1,52 @@
-var uv, manifest, urlDataProvider;
+let uv;
 
-window.addEventListener('uvLoaded', function(e) { 
+window.addEventListener('uvLoaded', function (e) {
 
-    urlDataProvider = new UV.URLDataProvider();
+    uv = createUV('#uv', {
+        iiifResourceUri: 'https://bfinationalarchivemanifest.bfi.org.uk/works/150784974/manifest.json',
+        configUri: 'uv-config.json'
+    }, new UV.URLDataProvider());
 
-    loadManifests(function() {
-        setSelectedManifest();
-        setupUV();
+    uv.on('showDownloadDialogue', function () {
+        logEvent('download_panel_opened', {
+            manifest: extractManifestId(uv),
+            canvas: extractCanvasId(uv)
+        });
+    });
+
+    uv.on('manifestIndexChanged', function (manifestIndex) {
+        logEvent('resource_loaded', {
+            uri: extractManifestId(uv)
+        })
+    });
+
+    uv.on('canvasIndexChanged', function (canvasIndex) {
+        logEvent('image_changed', {
+            manifest: extractManifestId(uv),
+            canvas: extractCanvasId(uv)
+        });
     });
 
 }, false);
 
-function setupUV() {
-
-    var collectionIndex = urlDataProvider.get('c');
-
-    var data = {
-        iiifResourceUri: manifest,
-        configUri: 'examples-config.json',
-        collectionIndex: (collectionIndex !== undefined) ? Number(collectionIndex) : undefined,
-        manifestIndex: Number(urlDataProvider.get('m', 0)),
-        sequenceIndex: Number(urlDataProvider.get('s', 0)),
-        canvasIndex: Number(urlDataProvider.get('cv', 0)),
-        rotation: Number(urlDataProvider.get('r', 0)),
-        rangeId: urlDataProvider.get('rid', ''),
-        xywh: urlDataProvider.get('xywh', '')
-    };
-
-    uv = createUV('#uv', data, urlDataProvider);
-
-    uv.on('created', function() {
-        Utils.Urls.setHashParameter('manifest', manifest);
-        console.log('parsed metadata', uv.extension.helper.manifest.getMetadata());
-        console.log('raw jsonld', uv.extension.helper.manifest.__jsonld);
-    });
-
+function extractManifestId(uv) {
+    return uv.extension.helper.manifest.id;
 }
 
-function loadManifests(cb) {
+function extractCanvasId(uv) {
+    return uv.extension.helper.getCurrentCanvas().id;
+}
 
-    // load manifests
-    $.getJSON('manifests.json', function(manifests) {
-
-        var $manifestSelect = $('#manifestSelect');
-
-        for (var i = 0; i < manifests.collections.length; i++) {
-            var collection = manifests.collections[i];
-
-            if (collection.visible === false) {
-                continue;
-            }
-
-            $manifestSelect.append('<optgroup label="' + collection.label + '">');
-
-            for (var j = 0; j < collection.manifests.length; j++) {
-                var manifest = collection.manifests[j];
-
-                if (manifest.visible !== false) {
-                    $manifestSelect.append('<option value="' + manifest['@id'] + '">' + manifest.label + '</option>');
-                }
-            }
-
-            $manifestSelect.append('</optgroup>');      
+function logEvent(type, payload) {
+    $.post({
+        url: '/api/event',
+        data: {
+            'type': type,
+            ...payload
+        },
+        dataType: 'json',
+        success: function() {
+            console.log('Successfully logged event [' + type + '] with payload [' + payload + ']');
         }
-
-        cb();
     });
 }
-
-function setSelectedManifest() {
-
-    manifest = Utils.Urls.getHashParameter('manifest');
-
-    if (manifest) {
-        $('#manifestSelect').val(manifest);
-    } else {
-        var options = $('#manifestSelect option');
-
-        if (options.length) {
-            manifest = options[0].value;
-        }
-    }
-
-    $('#manifest').val(manifest);
-}
-
-$('#manifestSelect').on('change', function() {
-    $('#manifest').val($('#manifestSelect option:selected').val());
-});
-
-$('#setManifestButton').on('click', function() {
-    manifest = $('#manifest').val();
-
-    uv.set({
-        iiifResourceUri: manifest,
-        collectionIndex: undefined,
-        manifestIndex: 0,
-        sequenceIndex: 0,
-        canvasIndex: 0
-    });
-});
